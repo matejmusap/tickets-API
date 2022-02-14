a35238a68948fd43087dcf32ed125f1cbc39b327
matejmusap <matej.musap@gmail.com>
2022-02-15 00:03:57 +0100

    Add exception on util

--- src/util/getUniqueCode.ts
+++ src/util/getUniqueCode.ts
@@ -1,12 +1,18 @@
 import * as queries from '../database';
+import { utilException } from '../exceptions';
 
 const getUniqueCode = async (): Promise<string> => {
-    const code: string = Math.random().toString(36).slice(2);
-    const checkHash = await queries.transactionsQueries.checkIfCode(code);
-    if (checkHash === 1) {
-        return await getUniqueCode();
-    } else {
-        return code;
+    try {
+        const code: string = Math.random().toString(36).slice(2);
+        const checkHash = await queries.transactionsQueries.checkIfCode(code);
+        if (checkHash === 1) {
+            return await getUniqueCode();
+        } else {
+            return code;
+        }
+    } catch (err) {
+        console.log(err);
+        throw err as utilException;
     }
 };
 
